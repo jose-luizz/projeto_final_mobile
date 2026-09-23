0677f70ad82342e02fdbdac551fd0243eef81294
jose-luizz <zeluizzz12345@gmail.com>
2026-09-23 09:49:49 -0300

    segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:math';
 
 void main() {
   runApp(const MeuApp());
@@ -111,7 +114,7 @@ class _SplashScreenState extends State<SplashScreen> {
       ));
      }
     }
-  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -130,14 +133,13 @@ class _SplashScreenState extends State<SplashScreen> {
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
-            ),
+            ), 
           ],
         ),
       ),
     );
   }
 }
-
 
 class LoginScreen extends StatefulWidget {
   final void Function(Color) mudarCor;
@@ -158,7 +160,7 @@ class _LoginScreenState extends State<LoginScreen> {
     if (!mounted) return;
     Navigator.pushReplacement(context,
       MaterialPageRoute(
-        builder: (context) => HomeScreen(mudarCor: widget.mudarCor);
+        builder: (context) => HomeScreen(mudarCor: widget.mudarCor)
       ),
     );
   }
@@ -194,11 +196,37 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 }
 
-
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   final void Function(Color) mudarCor;
-
   const HomeScreen({super.key, required this.mudarCor});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+
+  String nomePokemon = '';
+  String? spritePokemon;
+  bool carregando = true;
+
+  @override
+  void initState(){
+    super.initState();
+    buscarPokemon();
+  }
+
+  Future<void> buscarPokemon() async {
+
+    final id = Random().nextInt(15);
+    final url = Uri.parse('https://pokeapi.co/api/v2/pokemon/$id');
+    final resposta = await http.get(url);
+    final dados = jsonDecode(resposta.body);
+    setState(() {
+      nomePokemon = dados['name'];
+      spritePokemon = dados['sprites']['front_default'];
+      carregando = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -240,7 +268,34 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: GridView.builder(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Card(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: carregando ? const Center(child: CircularProgressIndicator()) : Row(
+                  children: [
+                    if(spritePokemon != null)
+                    Image.network(spritePokemon!, width: 56, height: 56),
+                    const SizedBox(width: 12),
+                    Expanded(
+                      child: Text(
+                        'Pokemon do dia: ${nomePokemon[0].toUpperCase()}${nomePokemon.substring(1)}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+      Expanded(
+      child: GridView.builder(
         padding: const EdgeInsets.all(16),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -283,9 +338,13 @@ class HomeScreen extends StatelessWidget {
           );
         },
       ),
-    );
+    ),
+    ],
+  ),
+  );
   }
 }
+
 
 
 class ProfileScreen extends StatefulWidget {
